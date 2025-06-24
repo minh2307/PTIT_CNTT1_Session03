3c5c62c68cfeed11ef2b42208bdc5b7aedf1fa7a
minh2307 <minh23923@gmail.com>
2025-06-24 21:43:25 +0700

    fix bai03

--- PTIT_CNTT1_Session03_Bai3.c
+++ PTIT_CNTT1_Session03_Bai3.c
@@ -5,7 +5,9 @@ int average_even(int* arr, int n) {
     int sum = 0;
     int count = 0;
     for (int i = 0; i < n; i++) {
-        arr[i] % 2 == 0 ? sum += arr[i] : "";
+        if (arr[i] % 2 == 0) {
+            sum += arr[i];
+        }
         count ++;
     }
     return sum / count;
